74d5c4359bc0a4cd12ec576eec099deff1f78362
lambdamix <ilya.klyuchnikov@gmail.com>
2013-05-26 00:22:12 +0400

    example with infinite lists

--- Tests.hs
+++ Tests.hs
@@ -18,7 +18,13 @@ prog = read
 	\ gApp1(Cons(x, xs), ys) = Cons(x, gApp2(xs, ys));\
 	\ gApp1(Nil(), ys) = ys;\
  	\ gApp2(Cons(x, xs), ys) = Cons(x, gApp1(xs, ys));\
-	\ gApp2(Nil(), ys) = ys;"
+	\ gApp2(Nil(), ys) = ys; \
+	\ fZeros()  = Cons(Z(), fZeros()); \
+	\  \
+	\  \
+	\  \
+	\  \
+	\  "
 
 
 subst :: Subst
@@ -31,16 +37,22 @@ subst = [
 inputs :: [Expr]
 inputs = map read ["gApp(x, y)", "gApp(gApp(x, y), z)", "gApp1(x, y)", "gApp1(gApp1(x, y), z)"]
 
+expInputs :: [Expr]
+expInputs = map read ["fZeros()", "gApp(fZeros(), fZeros())"]
+
 graph  = foldTree . buildTree (driveMachine prog)
 graph' = simplify . graph
 
-graphs  = map graph  inputs
-graphs' = map graph' inputs
+graphs  = map graph  (inputs)
+graphs' = map graph' (inputs)
 
 demo = putStrLn . printTree
 
 demos  = map demo graphs
 demos' = map demo graphs'
+
+expDemos  = map (demo . graph ) expInputs 
+expDemos' = map (demo . graph') expInputs
 
 tests = test (
 	["test" ~: "transform" ~: int prog (i // subst) ~=? intTree (graph i) subst | i <- inputs] ++

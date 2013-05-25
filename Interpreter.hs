f489075c888bf7193125e25c82cbeb8d809855f1
lambdamix <ilya.klyuchnikov@gmail.com>
2013-05-26 01:17:40 +0400

    big-step and small-step evaluators

--- Interpreter.hs
+++ Interpreter.hs
@@ -3,6 +3,7 @@ module Interpreter where
 import Data
 import DataUtil
 
+-- small-step int
 int :: Program -> Expr -> Expr
 int p e = until isValue (intStep p) e
 
@@ -21,3 +22,20 @@ intStep p (GCall gname (Ctr cname cargs : args)) =
 
 intStep p (GCall gname (e:es)) = 
 	(GCall gname (intStep p e : es))
+
+-- big-step int
+eval :: Program -> Expr -> Expr
+eval p (Ctr name args) = 
+    Ctr name (map (eval p) args)
+
+eval p (FCall name args) = 
+    eval p (body // zip vs args) where
+        (FDef _ vs body) = fDef p name
+
+eval p (GCall gname (Ctr cname cargs : args)) = 
+    eval p (body // zip (cvs ++ vs) (cargs ++ args)) where 
+        (GDef _ (Pat _ cvs) vs body) = gDef p gname cname
+
+eval p (GCall gname (arg:args)) = 
+    eval p (GCall gname (eval p arg:args))
+

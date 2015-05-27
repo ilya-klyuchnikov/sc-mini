61bc1ce800eeaefef112f3ad202e2b04f60d1ead
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2015-05-27 20:45:34 +0300

    in progress

--- Treeless1.hs
+++ Treeless1.hs
@@ -9,77 +9,64 @@ data Ctr = Ctr0 String | Ctr1 String Exp | Ctr2 String Exp Exp deriving (Show, E
 data Fun = FFun1 String Exp | GFun1 String String Exp
 type Program = [Fun]
 
-eval :: Program -> Exp -> Exp
-eval p e = 
-    case e of        
-        Ctr c -> Ctr (evalCtr p c)
-        FCall1 n ctr -> evalFCall1 p p n ctr
-        GCall1 gn ctr -> evalGCall1 p gn ctr        
+eval :: Exp -> Program -> Exp        
+eval (Ctr c) p         = Ctr (evalCtr c p)
+eval (FCall1 n ctr) p  = evalFCall1 p p n ctr
+eval (GCall1 gn ctr) p = evalGCall1 ctr p gn 
 
-evalCtr :: Program -> Ctr -> Ctr
-evalCtr p ctr =
-    case ctr of 
-        Ctr0 s   -> Ctr0 s
-        Ctr1 s e -> Ctr1 s (eval p e)
-        Ctr2 s e1 e2 -> Ctr2 s (eval p e1) (eval p e2)
+evalCtr :: Ctr -> Program -> Ctr
+evalCtr (Ctr0 s) p       = Ctr0 s
+evalCtr (Ctr1 s e) p     = Ctr1 s (eval e p)
+evalCtr (Ctr2 s e1 e2) p = Ctr2 s (eval e1 p) (eval e2 p)
 
-eval10 :: Program -> Exp -> Exp -> Exp
-eval10 p e fv1 =
-    case e of
-        FVar1 -> fv1
-        Ctr c -> Ctr (evalCtr10 p c fv1)
-        FCall1 n FVar1 -> evalFCall1 p p n fv1
-        FCall1 n ctr   -> evalFCall1 p p n ctr
-        GCall1 n FVar1 -> evalGCall1 p n fv1
-        GCall1 n ctr   -> evalGCall1 p n ctr
+eval10 :: Exp -> Program -> Exp -> Exp
+eval10 FVar1 p fv1 = fv1
+eval10 (Ctr c) p fv1 = Ctr (evalCtr10 c p fv1)
+eval10 (FCall1 n FVar1) p fv1 = evalFCall1 p p n fv1
+-- TODO - check via (isVar) -- should work!!
+eval10 (FCall1 n ctr) p fv1 = evalFCall1 p p n ctr
+eval10 (GCall1 n FVar1) p fv1 = evalGCall1 fv1 p n
+eval10 (GCall1 n ctr) p fv1 = evalGCall1 ctr p n 
 
-eval01 :: Program -> Exp -> Exp -> Exp
-eval01 p e gv1 =
-    case e of
-        GVar1 -> gv1
-        Ctr c -> Ctr (evalCtr01 p c gv1)
-        FCall1 n GVar1 -> evalFCall1 p p n gv1
-        FCall1 n ctr   -> evalFCall1 p p n ctr
-        GCall1 n GVar1 -> evalGCall1 p n gv1
-        GCall1 n ctr   -> evalGCall1 p n ctr
+eval01 :: Exp -> Program -> Exp -> Exp
+eval01 GVar1 p gv1 = gv1
+eval01 (Ctr c) p gv1 = Ctr (evalCtr01 c p gv1)
+eval01 (FCall1 n GVar1) p gv1 = evalFCall1 p p n gv1
+eval01 (FCall1 n ctr) p gv1  = evalFCall1 p p n ctr
+eval01 (GCall1 n GVar1) p gv1 = evalGCall1 gv1 p n
+eval01 (GCall1 n ctr) p gv1 = evalGCall1 ctr p n 
 
-evalCtr10 :: Program -> Ctr -> Exp -> Ctr
-evalCtr10 p c fv1 =
-    case c of 
-        Ctr0 s   -> Ctr0 s
-        Ctr1 s e -> Ctr1 s (eval10 p e fv1)
-        Ctr2 s e1 e2 -> Ctr2 s (eval10 p e1 fv1) (eval10 p e2 fv1)
+evalCtr10 :: Ctr -> Program -> Exp -> Ctr
+evalCtr10 (Ctr0 s) p fv1 = Ctr0 s
+evalCtr10 (Ctr1 s e) p fv1 = Ctr1 s (eval10 e p fv1)
+evalCtr10 (Ctr2 s e1 e2) p fv1 = Ctr2 s (eval10 e1 p fv1) (eval10 e2 p fv1)
 
-evalCtr01 :: Program -> Ctr -> Exp -> Ctr
-evalCtr01 p c gv1 =
-    case c of 
-        Ctr0 s   -> Ctr0 s
-        Ctr1 s e -> Ctr1 s (eval01 p e gv1)
-        Ctr2 s e1 e2 -> Ctr2 s (eval01 p e1 gv1) (eval01 p e2 gv1)
-
+evalCtr01 :: Ctr -> Program -> Exp -> Ctr
+evalCtr01 (Ctr0 s) p fv1 = Ctr0 s
+evalCtr01 (Ctr1 s e) p fv1 = Ctr1 s (eval01 e p fv1)
+evalCtr01 (Ctr2 s e1 e2) p fv1 = Ctr2 s (eval01 e1 p fv1) (eval01 e2 p fv1)
 
 evalFCall1 :: Program -> Program -> String -> Exp -> Exp
 evalFCall1 p p1 fname arg1 = 
     case p1 of
         (FFun1 n' e) : p' -> 
             if (n'==fname) 
-                then (eval10 p e arg1) 
+                then (eval10 e p arg1) 
                 else (evalFCall1 p p' fname arg1)
-        _ : p' -> 
+        (GFun1 _ _ _) : p' -> 
             (evalFCall1 p p' fname arg1)
 
-evalGCall1 :: Program -> String -> Exp -> Exp
-evalGCall1 p gn (Ctr (Ctr1 cn arg1)) = 
-    evalGCall1' p p gn cn arg1
+evalGCall1 :: Exp -> Program -> String -> Exp
+evalGCall1 (Ctr (Ctr1 cn arg1)) p gn = evalGCall1' p p gn cn arg1
 
 evalGCall1' :: Program -> Program -> String -> String -> Exp -> Exp
 evalGCall1' p p1 gn cn arg1 =
     case p1 of
         (GFun1 gn' cn' e) : p' -> 
             if (gn'==gn && cn'==cn) 
-                then (eval01 p e arg1) 
+                then (eval01 e p arg1) 
                 else (evalGCall1' p p' gn cn arg1)
-        _ : p' -> 
+        (FFun1 _ _) : p' -> 
             (evalGCall1' p p' gn cn arg1)
 
 
@@ -118,22 +105,22 @@ ctr s [e1, e2] = Ctr (Ctr2 s e1 e2)
 
 in1 :: Exp
 in1 = FCall1 "id1" (ctr "Nil" [])
-result1 = eval idProg in1
+result1 = eval in1 idProg
 test1 = result1 == Ctr (Ctr0 "Nil")
 
 in2 :: Exp
 in2 = GCall1 "pred" (ctr "Z" [ctr "Unit" []])
-result2 = eval predProg in2
+result2 = eval in2 predProg
 test2 = result2 == Ctr (Ctr1 "Z" (Ctr (Ctr0 "Unit")))
 
 in3 :: Exp
 in3 = GCall1 "pred" (ctr "S" [(ctr "Z" [ctr "Unit" []])])
-result3 = eval predProg in3
+result3 = eval in3 predProg
 test3 = result3 == Ctr (Ctr1 "Z" (Ctr (Ctr0 "Unit")))
 
 in4 :: Exp
 in4 = GCall1 "zero" (ctr "S" [(ctr "S" [(ctr "Z" [ctr "Unit" []])])])
-result4 = eval predProg in4
+result4 = eval in4 predProg
 test4 = result4 == Ctr (Ctr1 "Z" (Ctr (Ctr0 "Unit")))
 
 tests = [test1, test2, test3, test4]
